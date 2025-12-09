144aeacd46e96174ecced6c9defa00f269f53808
prathap-231121101082 <prathapselvamp@gmail.com>
2025-12-09 12:32:32 +0530

    Add files via upload
    day5

---
+++ Facebook.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook {
+
+	public static void main(String[] args) throws IOException {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        File source =driver.getScreenshotAs(OutputType.FILE);
+        File destination = new File("./prathap/amazonpage.png");
+        FileUtils.copyFile(source,destination);
+        // TODO Auto-generated method stub
+
+	}
+
+}
